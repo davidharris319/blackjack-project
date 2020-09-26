3025519e7220d2bb2ba91bf6f87f3f582585ab3a
David Harris <davidharris319@gmail.com>
2020-09-26 11:28:01 -0400

    added wireframe and pseudocode for user flow

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* Blackjack game flow
+
+* User enters player name.
+* User is asked how much they would like to bet.
+* User enters bet amount.
+* User receives two cards.
+* User asks if they would like to hit or stand.
+* If User clicks hit button, receive another card. User is asked again.
+* If User clicks stand button, Dealers cards are revealed. Closest to 21 wins.
+* If player wins, they get receive double what they bet.
+* If player loses, the lose what they bet. 
+* Play again?
+
+*/
